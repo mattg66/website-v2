bd8a84f4b8438e1098b56a71afa936b4223491e9
Matt Gaynor <matt@gaynor.tech>
2022-09-27 23:27:44 +0100

    added link

--- layouts/experienceTimeline.tsx
+++ layouts/experienceTimeline.tsx
@@ -14,10 +14,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentStyle={{ background: '#003db6', color: '#fff' }}
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         iconStyle={{ background: '#003db6', color: '#fff' }}
-                        icon={<CiscoLogo/>}
+                        icon={<CiscoLogo />}
                     >
                         <h1 className="text-2xl mb-2">High-Speed SP Network Testing</h1>
-                        <Image src='/media/rack1.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/rack1.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>Planned and deployed hardware layout</p>
                         <p>Worked with engineers to introduce delay into the network for testing</p>
                         <p>Optimised PSU connections due to high power demand</p>
@@ -28,10 +28,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentStyle={{ background: '#003db6', color: '#fff' }}
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         iconStyle={{ background: '#003db6', color: '#fff' }}
-                        icon={<CiscoLogo/>}
+                        icon={<CiscoLogo />}
                     >
                         <h1 className="text-2xl mb-2">5G Cloud Core Testing</h1>
-                        <Image src='/media/rack2.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/rack2.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>
                             Designed the physical deployment of a large scale 5G VNF and CNF testbed with over 50 planned servers being used.
                         </p>
@@ -50,10 +50,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentStyle={{ background: '#003db6', color: '#fff' }}
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         iconStyle={{ background: '#003db6', color: '#fff' }}
-                        icon={<CiscoLogo/>}
+                        icon={<CiscoLogo />}
                     >
                         <h1 className="text-2xl">Private 5G Proof of Concept</h1>
-                        <Image src='/media/p5g.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/p5g.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>Worked as part of a small taskforce to put together an industry first proof of conept for a private 5G solution.</p>
                         <p>I standardised testbed deployment for similar projects in the future, and worked closely with the project team to iron out networking and performance issues.</p>
                         <p>Technologies involved: NCS, IOS-XE, PTP, GPS, vDU, vCU, RU</p>
@@ -64,10 +64,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         date="June 2019 - September 2020"
                         iconStyle={{ background: '#fff', color: '#fff' }}
-                        icon={<F6thLogo/>}
+                        icon={<F6thLogo />}
                     >
                         <h1 className="text-2xl mb-2">Technical Theatre Technician</h1>
-                        <Image src='/media/pulling_cables.e3146164.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/pulling_cables.e3146164.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>
                             Planned and implemented a new structured cabling network to future proof the theatre for the rise in lighting and sound data over IP networks.
                         </p>
@@ -78,10 +78,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         date="June 2019 - September 2020"
                         iconStyle={{ background: '#fff', color: '#fff' }}
-                        icon={<F6thLogo/>}
+                        icon={<F6thLogo />}
                     >
                         <h1 className="text-2xl mb-2">Technical Theatre Technician</h1>
-                        <Image src='/media/bar_socket.0e194677.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/bar_socket.0e194677.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>
                             Planned and implemented a new structured cabling network to future proof the theatre for the rise in lighting and sound data over IP networks.
                         </p>
@@ -92,10 +92,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         date="June 2019 - September 2020"
                         iconStyle={{ background: '#fff', color: '#fff' }}
-                        icon={<F6thLogo/>}
+                        icon={<F6thLogo />}
                     >
                         <h1 className="text-2xl mb-2">Technical Theatre Technician</h1>
-                        <Image src='/media/rack.2399c0c9.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/rack.2399c0c9.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>
                             Planned and implemented a new structured cabling network to future proof the theatre for the rise in lighting and sound data over IP networks.
                         </p>
@@ -106,10 +106,15 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         date="June 2019 - September 2020"
                         iconStyle={{ background: '#fff', color: '#fff' }}
-                        icon={<img src='/media/online-design.fd1b02a4.png' className='timeline-icon'/>}
+                        icon={<img src='/media/online-design.fd1b02a4.png' className='timeline-icon' />}
                     >
                         <h1 className="text-2xl mb-2">Web Development</h1>
-                        <Link href="https://carllyonsstudio.com"><Image src='/media/cls.c1591680.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/></Link>
+                        <Link href="https://carllyonsstudio.com">
+                            <a>
+                                <Image src='/media/cls.c1591680.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
+                                <h2><u>Carl Lyons Studio</u></h2>
+                            </a>
+                        </Link>
                         <p>
                             Built using React and PHP, this website is my flagship project to date. Worked with the client to design a bespoke CMS to power the site, including features such as automatic watermark generation, and secure private vaults for holding client work.
                         </p>
@@ -120,10 +125,10 @@ const ExperienceTimeline: React.FC<{}> = ({ }) => {
                         contentArrowStyle={{ borderRight: '7px solid  #003db6' }}
                         date="June 2019 - September 2020"
                         iconStyle={{ background: '#fff', color: '#fff' }}
-                        icon={<F6thLogo/>}
+                        icon={<F6thLogo />}
                     >
                         <h1 className="text-2xl mb-2">Technical Theatre Technician</h1>
-                        <Image src='/media/orchestra_sound.b42bf72f.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"/>
+                        <Image src='/media/orchestra_sound.b42bf72f.jpg' loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain" />
                         <p>
                             Planned and implemented a new structured cabling network to future proof the theatre for the rise in lighting and sound data over IP networks.
                         </p>
