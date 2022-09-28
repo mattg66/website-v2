d85c571a98ae79332aac0899750ed4eb09a80323
Matt Gaynor <matt@gaynor.tech>
2022-09-28 12:34:23 +0100

    colour fixes

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,8 +11,8 @@ const Card: React.FC<{ className?: string, icon?: IconDefinition, title?: string
 
                 </div>
                 <div className="p-5">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                    <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{title}</h5>
+                    <div className="mb-3 font-normal text-gray-400">
                         {children}
                     </div>
                 </div>
