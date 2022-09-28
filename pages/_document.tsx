dd6bf4a76a41aee296abf9e71acf6353d3388ae8
Matt Gaynor <matt@gaynor.tech>
2022-09-28 12:26:32 +0100

    added title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,19 +4,20 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <title>Matt Gaynor - Computer Networks Undergraduate</title>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
-                <meta name="title" content="Matt Gaynor - Computer Networks Undergrad" />
+                <meta name="title" content="Matt Gaynor - Computer Networks Undergraduate" />
                 <meta name="description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
 
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://matt.gaynor.tech/" />
-                <meta property="og:title" content="Matt Gaynor - Computer Networks Undergrad" />
+                <meta property="og:title" content="Matt Gaynor - Computer Networks Undergraduate" />
                 <meta property="og:description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
                 <meta property="og:image" content="https://matt.gaynor.tech/media/background.8b54e259-0.jpg" />
 
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="matt.gaynor.tech" />
-                <meta property="twitter:title" content="Matt Gaynor - Computer Networks Undergrad" />
+                <meta property="twitter:title" content="Matt Gaynor - Computer Networks Undergraduate" />
                 <meta property="twitter:description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
                 <meta property="twitter:image" content="https://matt.gaynor.tech/media/background.8b54e259-0.jpg" />
             </Head>
