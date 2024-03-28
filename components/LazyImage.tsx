21b70f36d870f80337a08e3811277ff1a0801d1a
Matt Gaynor (matgayno) <matgayno@cisco.com>
2024-03-28 20:28:53 +0000

    Update LazyImage.tsx

--- components/LazyImage.tsx
+++ components/LazyImage.tsx
@@ -8,7 +8,7 @@ const LazyImage = (props: ImageProps) => {
             <div className="relative">
                 <Image {...props} loading="lazy" width="100%" height="100%" layout="responsive" objectFit="contain"
                     className={`transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
-                    onLoad={() => setLoading(false)} />
+                    onLoadingComplete={() => setLoading(false)} />
                 {isLoading && <div className="absolute inset-0 flex items-center justify-center"><SyncLoader color="#ffffff" /></div>}
             </div>
         </>
