6d3c8cee8cb8ecefad3bb1cd25ff83ca95527fc5
Matt Gaynor (matgayno) <matgayno@cisco.com>
2024-03-31 18:47:42 +0100

    add clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 
 import '../styles/index.css'
@@ -6,10 +6,18 @@ import '../styles/index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import { clarity } from 'react-microsoft-clarity';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        clarity.init('lpe2bicsgt')
+    }, [])
     return (
         <>
+            <Head>
+                <title>Matt Gaynor - Computer Networks Undergraduate</title>
+            </Head>
             <GoogleReCaptchaProvider
                 reCaptchaKey="6LfDztwZAAAAAMHH1M_JoQm9k8bj7MaopFXtsDSH"
                 scriptProps={{
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,6 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <title>Matt Gaynor - Computer Networks Undergraduate</title>
-
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
