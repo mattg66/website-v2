45cc3b1350d1234aed1c9642201afb2dfaeb0e0f
Matt Gaynor (matgayno) <matgayno@cisco.com>
2024-04-05 11:08:34 +0100

    update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,18 +7,18 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
-                <meta name="title" content="Matt Gaynor - Computer Networks Undergraduate" />
+                <meta name="title" content="Matt Gaynor - Consulting Network Engineer" />
                 <meta name="description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
 
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://matt.gaynor.tech/" />
-                <meta property="og:title" content="Matt Gaynor - Computer Networks Undergraduate" />
+                <meta property="og:title" content="Matt Gaynor - Consulting Network Engineer" />
                 <meta property="og:description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
                 <meta property="og:image" content="https://matt.gaynor.tech/media/background.8b54e259-0.jpg" />
 
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="matt.gaynor.tech" />
-                <meta property="twitter:title" content="Matt Gaynor - Computer Networks Undergraduate" />
+                <meta property="twitter:title" content="Matt Gaynor - Consulting Network Engineer" />
                 <meta property="twitter:description" content="I'm Matt, a motivated and enthusiastic individual with a passion for all things IT and AV" />
                 <meta property="twitter:image" content="https://matt.gaynor.tech/media/background.8b54e259-0.jpg" />
             </Head>
